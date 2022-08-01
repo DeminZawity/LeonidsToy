66338a0f345e59ed0d90ab9c7c096de325fa358e
DeminZawity <106280855+DeminZawity@users.noreply.github.com>
2022-08-01 18:45:03 -0500

    Completed exercises 2 and 3

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
-const toys = [
-  {
-    id: 1,
-    name: "Rubber Duck",
-    price: 2.99,
-    inventory: 50,
-  },
-  {
-    id: 2,
-    name: "Robot",
-    price: 39.99,
-    inventory: 30,
-  },
-  {
-    id: 3,
-    name: "Doll House",
-    price: 49.99,
-    inventory: 15,
-  },
-  {
-    id: 4,
-    name: "Teddy Bear",
-    price: "9.99",
-    inventory: 25,
-  },
-];
+// ---------------------------------------------------------------------------EXERCISE ONE
+// const toys = [
+//   {
+//     id: 1,
+//     name: "Rubber Duck",
+//     price: 2.99,
+//     inventory: 50,
+//   },
+//   {
+//     id: 2,
+//     name: "Robot",
+//     price: 39.99,
+//     inventory: 30,
+//   },
+//   {
+//     id: 3,
+//     name: "Doll House",
+//     price: 49.99,
+//     inventory: 15,
+//   },
+//   {
+//     id: 4,
+//     name: "Teddy Bear",
+//     price: "9.99",
+//     inventory: 25,
+//   },
+// ];
+// ---------------------------------------------------------------------------EXERCISE TWO AND THREE
+const rubberDuck = { id: 1, name: "Rubber Duck", price: 2.99, inventory: 50 };
+const robot = { id: 2, name: "Robot", price: 39.99, inventory: 30 };
+const dollHouse = { id: 3, name: "Doll House", price: 49.99, inventory: 15 };
+const teddyBear = { id: 4, name: "Teddy Bear", price: "9.99", inventory: 25 };
+const toys = [rubberDuck, robot, dollHouse, teddyBear];
+console.log(toys);
