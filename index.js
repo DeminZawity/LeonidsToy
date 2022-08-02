208beb11a9ae1bcc567e59fd98920327ef4b0de4
DeminZawity <106280855+DeminZawity@users.noreply.github.com>
2022-08-01 19:29:58 -0500

    completed excercise 4

--- index.js
+++ index.js
@@ -26,9 +26,19 @@
 //   },
 // ];
 // ---------------------------------------------------------------------------EXERCISE TWO AND THREE
+// const rubberDuck = { id: 1, name: "Rubber Duck", price: 2.99, inventory: 50 };
+// const robot = { id: 2, name: "Robot", price: 39.99, inventory: 30 };
+// const dollHouse = { id: 3, name: "Doll House", price: 49.99, inventory: 15 };
+// const teddyBear = { id: 4, name: "Teddy Bear", price: "9.99", inventory: 25 };
+// const toys = [rubberDuck, robot, dollHouse, teddyBear];
+// console.log(toys);
+// ---------------------------------------------------------------------------EXERCISE FOUR
 const rubberDuck = { id: 1, name: "Rubber Duck", price: 2.99, inventory: 50 };
 const robot = { id: 2, name: "Robot", price: 39.99, inventory: 30 };
 const dollHouse = { id: 3, name: "Doll House", price: 49.99, inventory: 15 };
 const teddyBear = { id: 4, name: "Teddy Bear", price: "9.99", inventory: 25 };
 const toys = [rubberDuck, robot, dollHouse, teddyBear];
-console.log(toys);
+
+for (const toy of toys) {
+  console.log(toy.name);
+}
